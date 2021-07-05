dd4cb96d683370c2df3c31a6b88fa6a2b2a6d3f8
hdurand22 <hdurand22@gmail.com>
2021-07-05 15:51:15 -0700

    hoist predictions exercise

---
+++ Fundamentals/hoist.js
@@ -0,0 +1,194 @@
+// 1
+console.log(hello);                                   
+var hello = 'world';                                 
+
+// As seen by interpreter:
+var hello;
+console.log(hello);
+var hello = 'world';
+
+// Predicted output:
+// undefined
+
+// 2
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// As seen by the interpreter:
+var needle = 'haystack';
+test();
+function test() {
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+// Predicted output:
+// undefined
+
+// 3
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+// As seen by the interpreter:
+// no change
+
+// Predicted output:
+// 'super cool'
+
+// 4
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+
+// As seen by the interpreter:
+var food = 'chicken';
+console.log(food);
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+eat();
+
+// Predicted output:
+// 'chicken'
+// 'half-chicken'
+
+// 5
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+// As seen by the interpreter:
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+mean();
+console.log(food);
+console.log(food);
+
+// Predicted output:
+// undefined
+// 'fish'
+// undefined
+// undefined
+
+// 6
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// As seen by the interpreter:
+console.log(genre);
+var genre = "disco";
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+rewind();
+console.log(genre);
+
+// Predicted output:
+// undefined
+// undefined
+// 'r&b'
+// 'disco'
+
+// 7
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// As seen by the interpreter:
+dojo = "san jose";
+console.log(dojo);
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+learn();
+console.log(dojo);
+
+// Predicted output:
+// 'san jose'
+// 'seattle'
+// 'burbank'
+// 'san jose'
+
+// 8
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+
+// As seen by the interpreter:
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+
+// Predicted output:
+// true
+// 'closed for now'
+
+
